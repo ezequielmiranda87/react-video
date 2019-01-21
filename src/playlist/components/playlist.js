23df99419f105216ec4216452524a12cdcbba2d9
ezex87 <ezequielmiranda87@gmail.com>
2019-01-21 15:45:11 +0100

    Ico and Play components

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Media from './media';
 import  './playlist.css';
+import Play from '../../icons/components/Play'
 
 function Playlist(props){
     const playlist1 =  props.data.categories[0].playlist
@@ -8,6 +9,7 @@ function Playlist(props){
     const playlist3 = props.data.categories[2].playlist
     return(
         <div className="Playlist">
+        <Play size ={50} color="blue" ></Play>
         <h1>Title</h1>
         {playlist1.map((element, index)=>{
             return <Media 
