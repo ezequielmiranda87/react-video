34db8ad26c3e1c27e73a6f3f66e28d8bbcc3868f
ezex87 <ezequielmiranda87@gmail.com>
2019-01-28 11:41:11 +0100

    Updates

---
+++ precache-manifest.80da70feb60fab7b5a3608048c281f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "025a20c942d09779d0f8",
+    "url": "/react-video/static/js/runtime~main.025a20c9.js"
+  },
+  {
+    "revision": "66cf9325cf9393477690",
+    "url": "/react-video/static/js/main.66cf9325.chunk.js"
+  },
+  {
+    "revision": "553a7ff5e150c9d69c41",
+    "url": "/react-video/static/js/1.553a7ff5.chunk.js"
+  },
+  {
+    "revision": "66cf9325cf9393477690",
+    "url": "/react-video/static/css/main.59e1afdf.chunk.css"
+  },
+  {
+    "revision": "7d112b3ffe2b2843cd374902b7247793",
+    "url": "/react-video/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ezequielmiranda87/react-video/precache-manifest.5d6035ae063764fc491ed29c62068082.js"
+  "/react-video/precache-manifest.80da70feb60fab7b5a3608048c281f19.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ezequielmiranda87/react-video/index.html", {
+workbox.routing.registerNavigationRoute("/react-video/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
