5bea8e9bbeb1994a11ec49edf80a491f54115c7a
ezex87 <ezequielmiranda87@gmail.com>
2019-01-31 20:54:15 +0100

    Component search created

---
+++ src/widgets/components/search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './search.css'
+
+const Search = () => (
+    <form action="" className="Search">
+        <input
+            type="text"
+            className="Search-input"
+            placeholder="Buscá tu video favorito"
+            
+        />
+    </form>
+)
+
+export default Search;
