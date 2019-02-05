848d41735ce2b18cf9af3fd703c0b28079fbaa37
ezex87 <ezequielmiranda87@gmail.com>
2019-02-05 11:18:03 +0100

    Search form finished

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './search.css'
 
-const Search = () => (
-    <form action="" className="Search">
+const Search = (props) => (
+    <form 
+        action=""
+        className="Search"
+        onSubmit={props.handleSubmit}
+        >
         <input
             type="text"
             className="Search-input"
             placeholder="Buscá tu video favorito"
-            
+            // Esta propiedad de react se utiliza para crear referencias a elementos html y evey eventos.
+            // Cuando el componente se renderiza se setea una referencia dentro de esta propiedad
+            ref = {props.setRef} 
+            onChange = {props.handleChange}
+            value = {props.value}
         />
     </form>
 )
--- src/widgets/container/search.js
+++ src/widgets/container/search.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
-import Search from '../components/search'
+import Search from '../components/search';
 
 class SearchContainer extends Component{
+    state={
+        value : "default"
+    }
+
+    // set referencias de elementos html del componente presentacional en el componente container
+    setInputRef = element => {
+        this.input = element;
+    }
+
+    handleInputChange = event =>{
+        this.setState({
+            value: event.target.value.replace(" ", "-")
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.input, "Submit")
+    }
+    
     render(){
         return(
-            <Search></Search>
+            <Search
+            handleSubmit={this.handleSubmit}
+            setRef = {this.setInputRef}
+            handleChange = {this.handleInputChange}
+            value={this.state.value}
+            >
+            </Search>
         )
     }
 }
